c857b4a9e7c2967f882066c9886b968d078f80f9
Elizabeth Kipp <liz@elizabethkipp.com>
2015-12-17 14:27:53 -0600

    entered formatted skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,6 +6,9 @@
 
 //Bio Object
 
+
+
+
 var bio = {
     'name': 'Elizabeth Kipp',
     'role': 'Wev Developer and Photographer',
@@ -19,65 +22,89 @@ var bio = {
     'pictureURL': 'images/fry.jpg',
     'welcomeMsg': 'I was not born with a gift but I learned perserverance',
     'skills': [
-        'Web Developer, Photographer, Momma'
+        'Web Developer', 'Photographer'
     ]
 };
 
 
+
+if (bio.skills.length > 0) {
+
+   $('#header').append(HTMLskillsStart); 
+
+   var formattedSkill = HTMLskills.replace('%data%', bio.skills[0]);
+   $('#skills').append(formattedSkill); 
+    formattedSkill = HTMLskills.replace('%data%', bio.skills[1]);
+   $('#skills').append(formattedSkill); 
+};
+
 //Work Object
 var work = {
-    'jobs': [{
+    'jobs': [
+      {
         'employer': 'Elizabeth Kipp Photography',
         'title': 'Owner',
         'location': 'Chicago, Il',
         'dates': 'February 2011 to present',
         'description': 'Photograph weddings, events as well as product. Full knowledge of Adobe Photoshop, Lightroom and Bridge'
-    } {
+      },
+      {
         'employer': 'George Street Photo and Video',
         'title': 'Lead Photographer',
-        'dates': 'April 2012 to present'
+        'dates': 'April 2012 to present',
         'description': 'Lead wedding photographer'
-    }]
-}
+      }
+   ],
+};
 
 //Projects Object
 var projects = {
-    'projects': [{
-            'title': 'portfolio',
-            'dates': 'November 2015',
-            'description': 'first large project completed in the Udacity Nanodegree program. Made use of HTML, CSS, Modals and Bootstrap showcasing my photography and web development skills',
-            'images': 'http://elizabethsn7.github.io/frameworks/test.html'
-        }
+    'projects': [
+      {
+         'title': 'portfolio',
+         'dates': 'November 2015',
+         'description': 'first large project completed in the Udacity Nanodegree program. Made use of HTML, CSS, Modals and Bootstrap showcasing my photography and web development skills',
+         'images': 'http://elizabethsn7.github.io/frameworks/test.html'
+      },
+      {
         'titel': 'Wedding Quiz',
         'dates': 'August 2015',
         'description': 'Created a quiz app using HTML, CSS, JQuery and JavaScript featuring my wedding photography. I was able to create a landing page as well as a results page.',
         'images': 'http://elizabethsn7.github.io/Wedding-Quiz/'
-    ]
-}
+      }
+    ],
+};
 
 //Education Object
 var education = {
-    'schools': [{
-        'school': 'Keller Graduate School of Management',
-        'location': 'Tinley Park, Il',
-        'degree': 'MBA',
-        'major': ['Marketing'],
-        'years': 2002
-    }, {
+    'schools': [
+    {
+      'school': 'Keller Graduate School of Management',
+      'location': 'Tinley Park, Il',
+      'degree': 'MBA',
+      'major': ['Marketing'],
+      'years': 2002 
+    },
+    {
         'name': 'Columbia College Chicago',
         'location': 'Chicago, Il',
         'degree': 'BA',
         'major': ['Photography']
-    }]
-    'Online Courses': [{
-        'title': 'JavaScript Basics',
+    }
+    ],
+
+    'Online Courses': [
+      {
+        'title': 'Front End Nanodegree',
         'school': 'Udacity',
         'dates': 2015,
         'url': 'https//www.udacity.com/course/ud804'
-    }]
+    }
+   ],
 }
 
-// $('#main').append(bio.name);
+
+//$('#main').append(bio.name);
 // $('#main').append(work['position']);
 // $('#main').append(education.name);
 
@@ -89,11 +116,11 @@ var education = {
 // var formattedGithub = HTMLgithub.replace('%data%', bio.github);
 // var formattedLocation = HTMLlocation.replace('%data%', bio.location);
 // var formattedTwitter = HTMLtwitter.replace('%data%', bio.twitter);
-// var formattedSkills = HTMLskills.replace('%data%', bio.skills);
+var formattedSkills = HTMLskills.replace('%data%', bio.skills);
 
 // $('#header').append(formattedMobile, formattedGithub, formattedLocation, formattedTwitter, formattedSkills);
 
 // $('#header').append(formattedContactGeneric, formattedMobile, formmattedEmail, formattedGithub, formattedLocation, formattedTwitter, formattedSkills);
-console.log(bio);
+
 
 
