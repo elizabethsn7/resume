8479c7472bd148efdd1d01fab58f8f923b90964e
Elizabeth Kipp <liz@elizabethkipp.com>
2015-12-27 17:12:01 -0600

    used jsBeautify to clean up code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,136 +2,127 @@
 // var formattedRole = HTMLheaderRole.replace('%data%', role);
 // var formattedName = HTMLheaderName.replace('%data%', name);
 // $('#header').prepend(formattedName, formattedRole)
-
 //Bio Object
 var bio = {
-    'name': 'Elizabeth Kipp',
-    'role': 'Wev Developer and Photographer',
-    'contacts': {
-        'mobile': '773-931-4424',
-        'email': 'liz@elizabethkipp.com',
-        'github': 'https://github.com/elizabethsn7',
-        'twitter': 'https://twitter.com/photogirl1977',
-        'location': 'Chicago'
-    },
-    'pictureURL': 'images/fry.jpg',
-    'welcomeMsg': 'I was not born with a gift but I learned perserverance',
-    'skills': [
-        'Web Developer', 'Photographer'
-    ]
+  'name': 'Elizabeth Kipp',
+  'role': 'Wev Developer and Photographer',
+  'contacts': {
+    'mobile': '773-931-4424',
+    'email': 'liz@elizabethkipp.com',
+    'github': 'https://github.com/elizabethsn7',
+    'twitter': 'https://twitter.com/photogirl1977',
+    'location': 'Chicago'
+  },
+  'pictureURL': 'images/fry.jpg',
+  'welcomeMsg': 'I was not born with a gift but I learned perserverance',
+  'skills': [
+    'Web Developer', 'Photographer'
+  ]
 };
 
 if (bio.skills.length > 0) {
-   $('#header').append(HTMLskillsStart); 
+  $('#header').append(HTMLskillsStart);
 
-   var formattedSkill = HTMLskills.replace('%data%', bio.skills[0]);
-   $('#skills').append(formattedSkill); 
+  var formattedSkill = HTMLskills.replace('%data%', bio.skills[0]);
+  $('#skills').append(formattedSkill);
 
-    formattedSkill = HTMLskills.replace('%data%', bio.skills[1]);
-   $('#skills').append(formattedSkill); 
+  formattedSkill = HTMLskills.replace('%data%', bio.skills[1]);
+  $('#skills').append(formattedSkill);
 };
 
 //Work Object
 var work = {
-    'jobs': [
-      {
-        'employer': 'Elizabeth Kipp Photography',
-        'title': 'Owner',
-        'location': 'Chicago, Il',
-        'dates': 'February 2011 to present',
-        'description': 'Photograph weddings, events as well as product. Full knowledge of Adobe Photoshop, Lightroom and Bridge'
-      },
-      {
-        'employer': 'George Street Photo and Video',
-        'title': 'Lead Photographer',
-        'location': 'Chicago, Il',
-        'dates': 'April 2012 to present',
-        'description': 'Lead wedding photographer'
-      },
-      {
-        'employer': 'Parker and Evan Kipp',
-        'title': 'Mommala',
-        'location': 'Chicago, IL',  
-        'dates': 'March 2006 to Present',      
-        'description': 'Play with, teach, console and encourage! Cook and clean and take to places.'
-      }
-   ],
+  'jobs': [{
+    'employer': 'Elizabeth Kipp Photography',
+    'title': 'Owner',
+    'location': 'Chicago, Il',
+    'dates': 'February 2011 to present',
+    'description': 'Photograph weddings, events as well as product. Full knowledge of Adobe Photoshop, Lightroom and Bridge'
+  }, {
+    'employer': 'George Street Photo and Video',
+    'title': 'Lead Photographer',
+    'location': 'Chicago, Il',
+    'dates': 'April 2012 to present',
+    'description': 'Lead wedding photographer'
+  }, {
+    'employer': 'Parker and Evan Kipp',
+    'title': 'Mommala',
+    'location': 'Chicago, IL',
+    'dates': 'March 2006 to Present',
+    'description': 'Play with, teach, console and encourage! Cook and clean and take to places.'
+  }],
 };
 
 function displayWork() {
-for(job in work.jobs) {
-  $('#workExperience').append(HTMLworkStart);
+  for (job in work.jobs) {
+    $('#workExperience').append(HTMLworkStart);
 
-  var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
-  formattedWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
-  formattedEmployerTitle = formattedEmployer + formattedWorkTitle;
-  formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
-  formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
-  formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
-  $('.work-entry:last').append(formattedEmployerTitle, formattedWorkDates, formattedWorkLocation, formattedWorkDescription);
-}
+    var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+    formattedWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+    formattedEmployerTitle = formattedEmployer + formattedWorkTitle;
+    formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
+    formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
+    formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+    $('.work-entry:last').append(formattedEmployerTitle, formattedWorkDates, formattedWorkLocation, formattedWorkDescription);
+  }
 };
 displayWork();
 $(document).click(function(loc) {
   var x = loc.pageX;
-  var y =loc.pageY;
+  var y = loc.pageY;
 
   logClicks(x, y);
-  });
-
+});
 
 //Projects Object
 var projects = {
-    'projects': [
-      {
-         'title': 'portfolio',
-         'dates': 'November 2015',
-         'description': 'first large project completed in the Udacity Nanodegree program. Made use of HTML, CSS, Modals and Bootstrap showcasing my photography and web development skills',
-         'images': 'http://elizabethsn7.github.io/frameworks/test.html'
-      },
-      {
-        'titel': 'Wedding Quiz',
-        'dates': 'August 2015',
-        'description': 'Created a quiz app using HTML, CSS, JQuery and JavaScript featuring my wedding photography. I was able to create a landing page as well as a results page.',
-        'images': 'http://elizabethsn7.github.io/Wedding-Quiz/'
-      }
-    ],
+  'projects': [{
+    'title': 'portfolio',
+    'dates': 'November 2015',
+    'description': 'first large project completed in the Udacity Nanodegree program. Made use of HTML, CSS, Modals and Bootstrap showcasing my photography and web development skills',
+    'images': 'http://elizabethsn7.github.io/frameworks/test.html'
+  }, {
+    'titel': 'Wedding Quiz',
+    'dates': 'August 2015',
+    'description': 'Created a quiz app using HTML, CSS, JQuery and JavaScript featuring my wedding photography. I was able to create a landing page as well as a results page.',
+    'images': 'http://elizabethsn7.github.io/Wedding-Quiz/'
+  }],
 };
-
 
 //Education Object
 var education = {
-    'schools': [
-    {
-      'school': 'Keller Graduate School of Management',
-      'location': 'Tinley Park, Il',
-      'degree': 'MBA',
-      'major': ['Marketing'],
-      'years': 2002 
-    },
-    {
-        'name': 'Columbia College Chicago',
-        'location': 'Chicago, Il',
-        'degree': 'BA',
-        'major': ['Photography']
-    }
-    ],
+  'schools': [{
+    'school': 'Keller Graduate School of Management',
+    'location': 'Tinley Park, Il',
+    'degree': 'MBA',
+    'major': ['Marketing'],
+    'years': 2002
+  }, {
+    'name': 'Columbia College Chicago',
+    'location': 'Chicago, Il',
+    'degree': 'BA',
+    'major': ['Photography']
+  }],
 
-    'Online Courses': [
-      {
-        'title': 'Front End Nanodegree',
-        'school': 'Udacity',
-        'dates': 2015,
-        'url': 'https//www.udacity.com/course/ud804'
-    }
-   ],
+  'Online Courses': [{
+    'title': 'Front End Nanodegree',
+    'school': 'Udacity',
+    'dates': 2015,
+    'url': 'https//www.udacity.com/course/ud804'
+  }],
 }
 
-$('#main').append(internationalizeButton);
-// function inName = {
-//   var internationalize = bio.name[1].toUpperCase();
-  
-// } 
+var name = 'Elizabeth Kipp';
+
+function inName(name) {
+  var internationalized = name;
+  names[1] = names[1].toUpperCase();
+  internationalized = names.join(" ");
+
+  return internationalized;
+
+  $('#main').append(internationalizeButton);
+}
 
 
 //$('#main').append(bio.name);
